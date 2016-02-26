633f9486dda75045fec16ec62284adab518f425e
Jimmy Petersen <jipe@dtu.dk>
2016-02-26 07:25:27 +0100

    Bundler, datastore, etc
    - Switched to using Bundler for ruby gems
    - Restructured common lib directory
    - Added mock datastore container
    - Added Solr dedup container

--- enqueueing/app/app.rb
+++ enqueueing/app/app.rb
@@ -1,22 +1,6 @@
 require 'bunny'
+require 'json'
+require 'datastore'
 require 'pg'
 
-interrupted = false
 
-Signal.trap(:INT) do
-  interrupted = true
-end
-  
-#conn = Bunny.new
-#conn.start
-
-#ch = conn.create_channel
-
-#req_x = ch.direct('datastore.export_requests')
-STDERR.puts 'Enqueueing ready.'
-
-sleep 1 until interrupted
-
-STDERR.puts 'Shutting down.'
-
-#conn.close
--- lib/ruby/datastore.rb
+++ lib/ruby/datastore.rb
@@ -1,4 +1,45 @@
 require 'bunny'
 
 class Datastore
+
+  def initialize(rabbitmq_url = ENV['RABBITMQ_URL'])
+    @rabbitmq_url = rabbitmq_url
+  end
+
+  def export_events(from_time, until_time, set)
+    raise unless block_given?
+
+    conn = Bunny.new(@rabbitmq_url)
+    conn.start
+
+    ch = conn.create_channel
+    
+    req_x = ch.direct('datastore.export_requests')
+    res_x = ch.direct("datastore.export_results.tupp.#{Time.now}", :auto_delete => true)
+    res_q = ch.queue(res_x, :auto_delete => true, :exclusive => true)
+    
+    export_complete = false
+
+    res_q.bind(res_x).subscribe do |delivery_info, metadata, data|
+      if data == 'EOR'
+        export_complete = true
+      else
+        yield data
+      end
+    end
+
+    req_x.publish(%{
+      {
+        "exchange"  : "#{res_x.name}",
+        "fromdate"  : "#{from_time}",
+        "untildate" : "#{until_time}",
+        "set"       : "#{set}",
+        "requestor" : "tupp"
+      }
+    })
+
+    sleep 1 until export_complete
+  ensure
+    conn.close unless conn.nil?
+  end
 end
